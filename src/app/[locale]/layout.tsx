39218b3a5ed1eb7f41f743d31fb30683e659d173
tvlisboa <tvlisboa@hotmail.com>
2025-09-22 00:26:20 -0300

    implementando footer

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,10 +7,10 @@ import { GeistSans } from "geist/font/sans";
 import { Toaster } from "sonner";
 import { getMessages } from "next-intl/server";
 
-const APP_NAME = "Biomob";
+const APP_NAME = "Fantasiado";
 const APP_DEFAULT_TITLE = "Biomob";
 const APP_TITLE_TEMPLATE = "%s";
-const APP_DESCRIPTION = "Biomob!";
+const APP_DESCRIPTION = "Fantasiado!";
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000"),
@@ -47,6 +47,11 @@ export const metadata: Metadata = {
     },
     description: APP_DESCRIPTION,
   },
+  icons: {
+    icon: "/img/LOGO_INSTITUTO_BRANCA.png",
+    shortcut: "/img/LOGO_INSTITUTO_BRANCA.png",
+    apple: "/img/LOGO_INSTITUTO_BRANCA.png",
+  }
 };
 
 export default async function RootLayout({
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -15,43 +15,115 @@ export const Footer = () => {
   }
 
   return (
-    <div className="flex flex-col m-auto py-[52px]">
-      <div className="md:flex items-center justify-center h-12 px-2 gap-x-4">
-        <div className="flex items-center justify-center h-[1.925rem] px-2 gap-x-4">
-          <Link href="https://www.instagram.com/biomobguia/" target="_blank">
-            <IoLogoInstagram className="text-lg text-text-grey" />
-          </Link>
+    <>
+      <section className="py-4 px-5 relative">
+        <div className="max-w-7.5 mx-auto">
+          <div className="grid grid-cols-5 gap-6">
 
-          <Link href="https://www.linkedin.com/company/biomob/" target="_blank">
-            <FaLinkedinIn className="text-lg text-text-grey" />
-          </Link>
+            <div className="flex flex-auto">
+              <ul className="space-y-2 text-sm text-900">
+                <li className="font-medium">Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+                <li>BioGuia Cultural</li>
+                <li>Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+              </ul>
+            </div>
 
-          <Link href="https://pt-br.facebook.com/biomobguia/" target="_blank">
-            <FaFacebookF className="text-lg text-text-grey" />
-          </Link>
 
-          <Link href="https://www.youtube.com/@biomobguia" target="_blank">
-            <FaYoutube className="text-lg text-text-grey" />
-          </Link>
+            <div className="flex flex-col">
+              <ul className="space-y-2 text-sm text-700">
+                <li className="font-medium">Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+              </ul>
+            </div>
 
-          <p className="text-lg text-text-gray">|</p>
-          <p className="t1 roboto-font text-text-grey">Baixe o app</p>
+
+            <div className="flex flex-col">
+              <ul className="space-y-2 text-sm text-700">
+                <li className="font-medium">Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+              </ul>
+            </div>
+
+
+            <div className="flex flex-col">
+              <ul className="space-y-2 text-sm text-700">
+                <li className="font-medium">Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+              </ul>
+            </div>
+
+
+            <div className="flex flex-col">
+              <ul className="space-y-2 text-sm text-700">
+                <li className="font-medium">Lorem ipsum dol</li>
+                <li>Lorem ipsum dol</li>
+              </ul>
+            </div>
+          </div>
+
+
+          <div className="absolute top-1/2 right-12 transform -translate-y-1/2">
+            <img
+              src="/img/seloneutro.png"
+              alt="Selo Biomob"
+              width={114}
+              height={114}
+              className="w-20 h-20 object-contain"
+            />
+          </div>
         </div>
+      </section>
 
-        <div className="flex items-center justify-center h-[1.925rem] px-2 gap-x-4">
-          <Link href="https://apps.apple.com/br/app/biomob/id1090156739">
-            <FaApple className="text-lg text-text-grey" />
-          </Link>
-          <Link href="https://play.google.com/store/apps/details?id=com.biomob.bioplus">
-            <FaGooglePlay className="text-lg text-text-gray" />
-          </Link>
-          <p className="text-lg text-gray">|</p>
+
+      <div className="flex flex-col m-auto py-[52px]">
+        <div className="md:flex items-center justify-center h-16 px-2 gap-x-4 border-t border-black-300">
+          <div className="md:flex items-center justify-center h-12 px-2 gap-x-4">
+            <div className="flex items-center justify-center h-[1.925rem] px-2 gap-x-4">
+              <Link href="https://www.instagram.com/biomobguia/" target="_blank">
+                <IoLogoInstagram className="text-lg text-text-grey" />
+              </Link>
+
+              <Link href="https://www.linkedin.com/company/biomob/" target="_blank">
+                <FaLinkedinIn className="text-lg text-text-grey" />
+              </Link>
+
+              <Link href="https://pt-br.facebook.com/biomobguia/" target="_blank">
+                <FaFacebookF className="text-lg text-text-grey" />
+              </Link>
+
+              <Link href="https://www.youtube.com/@biomobguia" target="_blank">
+                <FaYoutube className="text-lg text-text-grey" />
+              </Link>
+
+              <p className="text-lg text-text-gray">|</p>
+              <p className="t1 roboto-font text-text-grey">Baixe o app</p>
+            </div>
+
+            <div className="flex items-center justify-center h-[1.925rem] px-2 gap-x-4">
+              <Link href="https://apps.apple.com/br/app/biomob/id1090156739">
+                <FaApple className="text-lg text-text-grey" />
+              </Link>
+              <Link href="https://play.google.com/store/apps/details?id=com.biomob.bioplus">
+                <FaGooglePlay className="text-lg text-text-gray" />
+              </Link>
+              <p className="text-lg text-gray">|</p>
+            </div>
+
+            <p className="">R. Afrânio de Melo Franco, 333 - Quitandinha - Petrópolis/RJ - CEP: 25651-000</p>
+            <button onClick={scrollToTop}>
+              <BsArrowUpCircleFill className="text-5xl text-grey-800" />
+            </button>
+          </div>
         </div>
-
-        <button onClick={scrollToTop}>
-          <BsArrowUpCircleFill className="text-5xl text-grey-800" />
-        </button>
       </div>
-    </div>
+    </>
   );
 };
